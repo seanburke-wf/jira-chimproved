a85db25cad271b030df67f91f743aabc3485b0f8
Sean Burke <sean.burke@workiva.com>
2016-04-14 16:38:16 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name            JIRA chImproved
+// @version         0.1.0
+// @namespace       https://github.com/leftmostcat/
+// @description     Add handy functionality to the JIRA Agile/Kanban boards
+// @include         *
+// ==/UserScript==
